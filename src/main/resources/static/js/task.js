ea4278f8578a9bdeca4313384b50924956bf5578
Pitisha96 <petrikin1996@gmail.com>
2021-10-16 22:21:34 +0300

    added view rating

--- src/main/resources/static/js/task.js
+++ src/main/resources/static/js/task.js
@@ -1,8 +1,13 @@
 let md = window.markdownit()
-
+let stars = $('.unvoted').children()
+let rating;
 $(document).ready(function (){
-    console.log('value')
+    rating=0;
     $('#content-box').append($(md.render($('#content').val())))
+    $('.rating').children().each(function (){
+        if(this.classList.contains('checked'))
+            rating+=1
+    })
 })
 
 $('.img-button').click(function (){
@@ -34,3 +39,30 @@ $('#btn-send').click(function (){
             ))
     })
 })
+
+stars.each(handleMouseOverRating)
+
+$('.rating').mouseout(function (){
+    stars.each(function (i){
+        $(this).removeClass('checked')
+        if(rating>=i+0.5)
+            $(this).addClass('checked')
+    })
+
+})
+
+function handleMouseOverRating(idx,e){
+    $(this).mouseover(function (){
+        stars.removeClass('checked')
+        for(let i=0;i<=idx;i++){
+            if(!stars.get(i).classList.contains('checked')){
+                stars.get(i).classList.add('checked')
+            }
+        }
+    })
+}
+
+
+
+
+
